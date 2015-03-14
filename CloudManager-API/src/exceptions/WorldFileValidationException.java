536f0d99947d71fe934dec2143952e37e5e7b1b9
ddelilah <delia.balaj@yahoo.com>
2015-03-14 15:22:23 +0200

    CloudManager-API (Mirel)

---
+++ CloudManager-API/src/exceptions/WorldFileValidationException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class WorldFileValidationException extends Exception {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 9117746756595490018L;
+
+	public WorldFileValidationException(String message) {
+        super(message);
+    }
+
+}
